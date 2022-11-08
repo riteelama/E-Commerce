2c40e7605b0fda95dfb7845ad463d0e55d53efae
Alok <alokgo@gmail.com>
2022-11-08 20:27:57 +0545

    "Added the Catalog Manager for the API"

---
+++ CatalogService/BusinessManager/CatalogManager.cs
@@ -0,0 +1,21 @@
+﻿using CommonEnitity.Catalog;
+
+namespace CatalogService.BusinessManager;
+
+public class CatalogManager
+{
+    public IEnumerable<CatalogItem> GetCatalogItemList()
+    {
+        List<CatalogItem> objList = new List<CatalogItem>();
+        //We will call SQLData Provider to get the data from MS SQL DB
+        return objList;
+    }
+
+    public CatalogItem GetCatalogItemByID(Guid itemID)
+    {
+        CatalogItem objItem = new CatalogItem();
+
+        return objItem;
+    }
+}
+
---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,54 @@
+﻿using CatalogService.BusinessManager;
+using CommonEnitity.Catalog;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        // GET: api/<CatalogController>
+        [HttpPost]
+        public IEnumerable<CatalogItem> GetCatalogItemList()
+        {
+            CatalogManager catalogManager = new CatalogManager();            
+            return catalogManager.GetCatalogItemList();
+        }
+
+        [HttpPost]
+        public CatalogItem GetCatalogItem(Guid CatalogItemID)
+        {
+            CatalogManager catalogManager = new CatalogManager();
+            return catalogManager.GetCatalogItemByID(CatalogItemID);
+        }
+
+        // GET api/<CatalogController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CatalogController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CatalogController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CatalogController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
